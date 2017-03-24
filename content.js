f9d0ca404b008d2e0279dec8876ad99c340640d5
Noah Kittleson <noahkittleson@gmail.com>
2017-03-23 20:05:10 -0700

    it deletes youtube comments

--- content.js
+++ content.js
@@ -1,2 +1,6 @@
-var elem = document.getElementById("bannerandheader");
-elem.innerHTML = "Comment section deleted by RadioSilence";
+var elem = document.getElementById("watch-discussion");
+var fuckingFUCKelem = document.createElement('p');
+var fuckingFUCKtextNODE = document.createTextNode('Comment section deleted by RadioSilence');
+fuckingFUCKelem.appendChild(fuckingFUCKtextNODE);
+elem.parentNode.appendChild(fuckingFUCKelem);
+elem.parentNode.removeChild(elem);
