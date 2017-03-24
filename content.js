b7778e41d037a692903b3c016256b4561196c7a3
Noah Kittleson <noahkittleson@gmail.com>
2017-03-23 18:01:30 -0700

    added jquery dependency

---
+++ content.js
@@ -0,0 +1,6 @@
+// var element = document.getElementById("desktopNav")
+// element.html('<p>Comment Section blocked by RadioSilence.</p>');
+
+
+var replaced = $("body").html().replace(/text/g,'replace');
+$("body").html('');
