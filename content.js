7151b8564077b65cf5f4b961354154e863b7b45a
Noah Kittleson <noahkittleson@gmail.com>
2017-03-23 19:27:34 -0700

    finally replaced a specific div and deletion announcement

--- content.js
+++ content.js
@@ -1,14 +1,2 @@
-// var element = document.getElementById("desktopNav");
-// console.print(element);
-// element.html('<p>Comment Section blocked by RadioSilence.</p>');
-// alert("fuck");
-
-
-// document.body.innerHTML = document.body.innerHTML.getElementById("bannerandheader").html('');
-//
-// var test = document.body.innerHTML;
-// var element = test.getElementById("bannerandheader");
-// element.html('');
-
 var elem = document.getElementById("bannerandheader");
-elem.parentNode.removeChild(elem);
+elem.innerHTML = "Comment section deleted by RadioSilence";
