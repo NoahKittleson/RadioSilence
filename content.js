7931c2b76891ae0cf24682c906fb2bf96140b269
Noah Kittleson <noahkittleson@gmail.com>
2017-03-23 21:52:28 -0700

    stops checking for comment sections after 1 minute

--- content.js
+++ content.js
@@ -1,6 +1,34 @@
-var elem = document.getElementById("watch-discussion");
-var fuckingFUCKelem = document.createElement('p');
-var fuckingFUCKtextNODE = document.createTextNode('Comment section deleted by RadioSilence');
-fuckingFUCKelem.appendChild(fuckingFUCKtextNODE);
-elem.parentNode.appendChild(fuckingFUCKelem);
-elem.parentNode.removeChild(elem);
+fuckOFFid('watch-discussion');
+
+var tick = 0
+for (var i = 0; i < 60; i++) {
+    setTimeout(function() {
+      fuckOFFclass('fyre-widget');
+      console.log(tick++ + " cycles");
+    }, 1000 * i);
+}
+
+
+function fuckOFFid(fuckTHISone) {
+  var elem = document.getElementById(fuckTHISone);
+  if(!elem) return;
+  var fuckingFUCKelem = document.createElement('p');
+  var fuckingFUCKtextNODE = document.createTextNode('Comment section deleted by RadioSilence');
+  fuckingFUCKelem.appendChild(fuckingFUCKtextNODE);
+  elem.parentNode.appendChild(fuckingFUCKelem);
+  elem.parentNode.removeChild(elem);
+}
+
+function fuckOFFclass(fuckTHISone) {
+  var elems = document.getElementsByClassName("fyre-widget");
+  if(!elems) return;
+  var fuckingFUCKelem = document.createElement('p');
+  var fuckingFUCKtextNODE = document.createTextNode('Comment section deleted by RadioSilence');
+  fuckingFUCKelem.appendChild(fuckingFUCKtextNODE);
+  console.log(elems);
+
+  for(var i=0; i<elems.length; i++) {
+    elems[i].parentNode.appendChild(fuckingFUCKelem);
+    elems[i].parentNode.removeChild(elems[i])
+  }
+}
